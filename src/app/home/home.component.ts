d53dd61c9e1c373212062a1d5a700a8a4e5a8e46
bhanjanmelorra <bhanjan.behera@melorra.com>
2018-11-26 23:10:02 +0530

    slider

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,6 +10,22 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    const img_icon = document.getElementById('img-con');
+    const next = document.getElementById('next');
+    const prev = document.getElementById('prev');
+    const images = ['la.jpg','chicago.jpg','ny.jpg'];
+
+    let i = images.length;
+
+    next.onclick = () => { 
+      i = (i<images.length) ? (i=i+1):(i=1);
+      img_icon.innerHTML = "<img class='img' src =assets/images/"+images[i-1]+ ">";
+    }
+
+    prev.onclick = () => {
+      i = (i<images.length && i >1) ? (i=i-1):(i=images.length);
+      img_icon.innerHTML = "<img class='img' src=assets/images/"+images[i-1]+ ">";
+    }
   }
 
 }
