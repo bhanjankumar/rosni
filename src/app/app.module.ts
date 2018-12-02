7520d1e99a3326f128fbae79f5fc31067d867607
bhanjanmelorra <bhanjan.behera@melorra.com>
2018-12-02 21:44:02 +0530

    angular reactive form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HerosComponent } from './heros/heros.component';
+import { RouterModule, Routes } from '@angular/router';
+import { FormComponent } from './form/form.component';
+//mport { formControlBinding } from '@angular/forms/src/directives/reactive_directives/form_control_directive';
 
+const appRoutes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: 'heros', component: HerosComponent },
+  { path: 'forms', component: FormComponent }
+];
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    HerosComponent
+    HerosComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes),
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/form/form.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+export class FormComponent implements OnInit {
+
+  registerForm: FormGroup;
+  submitted = false;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+      this.registerForm = this.formBuilder.group({
+          firstName: ['', Validators.required],
+          lastName: ['', Validators.required],
+          email: ['', [Validators.required, Validators.email]],
+          password: ['', [Validators.required, Validators.minLength(6)]]
+      });
+  }
+
+  // convenience getter for easy access to form fields
+  get f() { return this.registerForm.controls; }
+
+  onSubmit() {
+      this.submitted = true;
+      console.log(this.registerForm.value)
+      // stop here if form is invalid
+      if (this.registerForm.invalid) {
+          return;
+      }
+  }
+
+}
